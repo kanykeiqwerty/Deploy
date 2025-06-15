5ada3007e760167ad63fc3c008e5db86200f8216
kanykeiqwerty <ghjvgyjvgyj@gmail.com>
2025-06-15 20:01:51 +0600

    aa

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/kanykei/Desktop/projects/VibeHackathon/venv
+set -gx VIRTUAL_ENV /home/kanykei/Desktop/projects/deploy/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
